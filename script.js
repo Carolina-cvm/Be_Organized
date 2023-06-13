7895baac6ca1082a5056b086f2773bb7db4fe7d7
Carolina Martins <carolina.martins3@pucpr.edu.br>
2023-06-13 17:47:13 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
+// DATE
 date = new Date().toLocaleDateString();
 document.getElementById("current_date").innerHTML = date;
+
+// TIME
+const h = new Date();
+let hour = h.getHours();
+
+const m = new Date();
+let minutes = m.getMinutes();
+
+const s = new Date();
+let seconds = s.getSeconds(); 
+
+document.getElementById("current_time").innerHTML = hour + ":" + minutes + ":" + seconds;
